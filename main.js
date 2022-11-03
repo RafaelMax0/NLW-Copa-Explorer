74ba91a8cb257386b1a5f9b137579d1c55694487
RafaelMax0 <rafaelmaxdev@gmail.com>
2022-11-03 17:19:02 -0300

    "row"

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function createGame(player1, hour, player2) {
-    let player1art = player1.slice(-1) === 'a' ? 'da' : 'do';
-    let player2art = player2.slice(-1) === 'a' ? 'da' : 'do';                       
-    return `
+  let player1art = player1.slice(-1) === "a" ? "da" : "do"
+  let player2art = player2.slice(-1) === "a" ? "da" : "do"
+  return `
     <li>
     <img src="./assets/icon-${player1}.svg" alt="Bandeira ${player1art} ${player1}">
     <strong>${hour}</strong>
@@ -9,10 +9,11 @@ function createGame(player1, hour, player2) {
 </li>
     `
 }
-
-function createCard (date, day, games) {
-    return `
-    <div class = "card">
+let delay = 0
+function createCard(date, day, games) {
+  delay = delay + 0.4
+  return `
+    <div class = "card" style ="animation-delay: ${delay}">
     <h2>${date} <span>${day}</span></h2>
     <ul>
     ${games}
@@ -21,9 +22,12 @@ function createCard (date, day, games) {
 
     `
 }
-
-document.querySelector('#cards').innerHTML = 
-createCard('24/11', 'quinta', createGame('brazil', '16:00', 'serbia')) +
-createCard('28/11', 'segunda', createGame('switzerland', '13:00', 'brazil') +
-createGame('portugal', '16:00', 'uruguay')) +
-createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))
+document.querySelector("#cards").innerHTML =
+  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("switzerland", "13:00", "brazil") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
